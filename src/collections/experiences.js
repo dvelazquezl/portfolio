096b1c0efa6e14a9e449e17cc3d699d6b1e431df
Daniel Velázquez <daniel545mjj@gmail.com>
2024-11-01 16:48:28 -0300

    update work experience

--- src/collections/experiences.js
+++ src/collections/experiences.js
@@ -1,16 +1,18 @@
 export const experiences = [
-	{
-		"dates": "March 2021 · Present",
-		"role": "Front-end Engineer",
-		"company": "10 Grounds",
-		"description": "Responsible for customer service and CRM system front-end development.",
-		"logo": "/assets/images/experiences/10g.png"
-	},
-	{
-		"dates": "March 2020 · December 2020",
-		"role": "Web Developer",
-		"company": "Universidad Nacional de Itapúa",
-		"description": "Internship",
-		"logo": "/assets/images/experiences/uni.png"
-	}
-]
+  {
+    dates: "March 2021 · Present",
+    role: "Front-end Engineer",
+    company: "10 Grounds",
+    description:
+      "Working as a contractor helping the team maintain, design and develop new features for an ap that is used by thousands of people.",
+    logo: "/assets/images/experiences/10g.png",
+  },
+  {
+    dates: "March 2020 · December 2020",
+    role: "Web Developer",
+    company: "Universidad Nacional de Itapúa",
+    description:
+      "Worked as an Internship. Developed a new version of a File Management System for specific documents.",
+    logo: "/assets/images/experiences/uni.png",
+  },
+];
