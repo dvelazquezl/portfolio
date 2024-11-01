8b7d8078973380847c9eb47a26328f7d8a51c2ef
Daniel Velázquez <daniel545mjj@gmail.com>
2024-11-01 01:59:54 -0300

    updated some assets and information

---
+++ src/collections/experiences.js
@@ -0,0 +1,16 @@
+export const experiences = [
+	{
+		"dates": "March 2021 · Present",
+		"role": "Front-end Engineer",
+		"company": "10 Grounds",
+		"description": "Responsible for customer service and CRM system front-end development.",
+		"logo": "/assets/images/experiences/10g.png"
+	},
+	{
+		"dates": "March 2020 · December 2020",
+		"role": "Web Developer",
+		"company": "Universidad Nacional de Itapúa",
+		"description": "Internship",
+		"logo": "/assets/images/experiences/uni.png"
+	}
+]
---
+++ src/collections/menus.js
@@ -0,0 +1,14 @@
+export const menus = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+];
