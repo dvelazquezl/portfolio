ff07a227d7fee5b4c5f7cb5215b71877590d9d9f
Daniel Velázquez <daniel545mjj@gmail.com>
2024-11-08 14:44:15 -0300

    delete unnecessary files, convert images png to webp

--- src/collections/projects.js
+++
@@ -1,8 +0,0 @@
-export const projects = [
-  {
-    name: "Weather Forecast",
-    description: "Minimal Weather Forecast",
-    image: "/assets/images/projects/weather-forecast.png",
-    url: "https://small-weather-forecast.vercel.app/",
-  },
-];
