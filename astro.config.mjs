a21abae06543e3ef384297080fbb48dca1d5eb85
Daniel Velázquez <daniel545mjj@gmail.com>
2024-11-11 15:44:33 -0300

    add internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  base: "/",
+  build: {
+    format: "directory",
+  },
+  site: "https://danielvelazquez.vercel.app",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,55 @@
+export const defaultLang = "en";
+
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const ui = {
+  en: {
+    "layout.title": "Daniel Velázquez - Software Engineer",
+    "layout.description": "Software Engineer from Paraguay",
+    "hero.title": "Hi, I'm Daniel",
+    "hero.subtitle1": "I'm a Software Engineer living in Obligado, Paraguay.",
+    "hero.subtitle2": "I focus on Web and Mobile development.",
+    "hero.cta": "Let's talk",
+    "experience.title": "Experience",
+    "experience.jobs.frontend.role": "Frontend Developer",
+    "experience.jobs.frontend.company": "10 Grounds",
+    "experience.jobs.frontend.dates": "March 2021 - Present",
+    "experience.jobs.frontend.description":
+      "Working as a contractor helping the team maintain, design and develop new features for a social media app that is used by thousands of people.",
+    "experience.jobs.web.role": "Web Developer",
+    "experience.jobs.web.company": "Universidad Nacional de Itapúa",
+    "experience.jobs.web.dates": "March 2020 - December 2020",
+    "experience.jobs.web.description":
+      "Worked as an Internship. Developed a new version of a File Management System for specific documents.",
+    "about.title": "About me",
+    "about.description":
+      "👋 I'm a software engineer from San Pedro del Paraná, Paraguay. I'm passionate about building new products and learning new technologies. I also love to play video games 🎮 with my friends.",
+  },
+  es: {
+    "layout.title": "Daniel Velázquez - Ingeniero Informático",
+    "layout.description": "Un Ingeniero Informático Paraguayo",
+    "hero.title": "Hey, Soy Daniel",
+    "hero.subtitle1":
+      "Soy un Ingeniero Informático que vive en Obligado, Paraguay.",
+    "hero.subtitle2": "Enfocado en el desarrollo Web y Móvil",
+    "hero.cta": "Contáctame",
+
+    "experience.title": "Experiencia",
+    "experience.jobs.frontend.role": "Desarrollador Frontend",
+    "experience.jobs.frontend.company": "10 Grounds",
+    "experience.jobs.frontend.dates": "Marzo 2021 - Presente",
+    "experience.jobs.frontend.description":
+      "Working as a contractor helping the team maintain, design and develop new features for a social media app that is used by thousands of people.",
+    "experience.jobs.web.role": "Desarrollador Web",
+    "experience.jobs.web.company": "Universidad Nacional de Itapúa",
+    "experience.jobs.web.dates": "Marzo 2020 - Diciembre 2020",
+    "experience.jobs.web.description":
+      "Worked as an Internship. Developed a new version of a File Management System for specific documents.",
+    "about.title": "Sobre mí",
+    "about.description":
+      "👋 I'm a software engineer from San Pedro del Paraná, Paraguay. I'm passionate about building new products and learning new technologies. I also love to play video games 🎮 with my friends.",
+  },
+} as const;
